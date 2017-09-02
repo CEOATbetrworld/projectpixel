64d40d29b35959a321c73a40c88e843b76d166ce
DHRUV KUMAR <dkiphones620@gmail.com>
2017-09-02 12:07:16 +0530

    please help

--- designs.js
+++ designs.js
@@ -6,6 +6,7 @@
 function makeGrid() {
 // after Upadating my course of FEND I found this project very difficult I am unable to implement what is written here
 // Please let me know about the resources from where I can learn how to complete these projects
+//Please either downgrade my nanodegree or let me from where I can learn to build these projects.
 // Your code goes here!
 
 }
