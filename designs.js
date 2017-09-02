02e61688ebc75eb1f38eccbea1a9998410b76ae8
DHRUV KUMAR <dkiphones620@gmail.com>
2017-09-03 00:58:40 +0530

    learning

--- designs.js
+++ designs.js
@@ -1,10 +1,28 @@
 // Select color input
+
 // Select size input
 
+
 // When size is submitted by the user, call makeGrid()
+document.getElementById("give").addEventListener("click",makeGrid);
 
 function makeGrid() {
+	let rows = document.getElementById('input_height').value;
+    let  columns = document.getElementById('input_width').value;
+   console.log(rows , columns);
+// Your code goes here
 
-// Your code goes here!
+ table = document.getElementById("pixel_canvas");
 
-}
+  for (let i=1; i < rows; i++) 
+  {
+    row = table.insertRow();  // DOM method for creating table rows
+    for(let j = 0 ; j < columns ; j++)
+    {
+    row.insertCell()
+    }
+  }
+  
+
+
+ 
